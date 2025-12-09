a2b96985530845970604b6b6c43919c4f0b1fa48
n1codev <br.pinoa@gmail.com>
2025-12-09 09:06:15 -0300

    Commit final  prueba

--- PruebaTecnicaCarsales.Domain/Character.cs
+++ PruebaTecnicaCarsales.Domain/Character.cs
@@ -13,11 +13,11 @@ namespace PruebaTecnicaCarsales.Domain
         public required string Status { get; init; } // ej. Alive, Dead, unknown
         public required string Species { get; init; }
         public required string Gender { get; init; }
-        public required string ImageUrl { get; init; } // La URL de la imagen del personaje
+        public required string ImageUrl { get; init; } //Imagen personaje
         public required string OriginName { get; init; }
         public required string LocationName { get; init; }
         public required List<string> EpisodeUrls { get; init; }
-        //public required DateTime Created { get; init; }
+   
         public required string Created { get; init; }
 
     }
